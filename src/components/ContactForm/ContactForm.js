1c90658eab6a2e429347db21e4bc574b686dca76
Victor8825 <victorrv2588@gmail.com>
2022-08-19 12:45:19 +0300

    Revert "rewrote ContactForm component with Formik library"
    This reverts commit 61d32b8d2c35d59716413f6127c11bf245a6dadb.

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,56 +1,62 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as yup from 'yup';
-
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { StyledContactForm } from './StyledContactForm';
 import PropTypes from 'prop-types';
 
-const initialValues = {
-  name: '',
-  number: '',
-};
-
-const schema = yup.object().shape({
-  name: yup.string().required('Name is required'),
-  number: yup.number().required('A phone number is required'),
-});
-
-export const ContactForm = ({ addContact }) => {
-  const handleSubmit = (values, actions) => {
-    const newValues = values;
-    addContact({
-      id: nanoid(),
-      name: newValues.name,
-      number: newValues.number,
-    });
-    actions.resetForm();
+export class ContactForm extends Component {
+  static defaultProps = {
+    addContact: PropTypes.func.isRequired,
   };
 
-  return (
-    <StyledContactForm>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={schema}
-        onSubmit={handleSubmit}
-      >
-        <Form autoComplete="off">
+  state = {
+    name: '',
+    number: '',
+  };
+
+  onInputChange = event => {
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+  };
+
+  addNewContactToPhonebook = event => {
+    event.preventDefault();
+    this.props.addContact({
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    });
+    event.currentTarget.reset();
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <StyledContactForm>
+        <form onSubmit={this.addNewContactToPhonebook} autoComplete="off">
           <label>
             <span>Name</span>
-            <Field type="tel" name="name" />
-            <ErrorMessage name="name" component="div" />
+            <input
+              type="tel"
+              name="name"
+              onChange={this.onInputChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
           </label>
           <label>
             <span>Number</span>
-            <Field type="tel" name="number" />
-            <ErrorMessage name="number" component="div" />
+            <input
+              type="tel"
+              name="number"
+              onChange={this.onInputChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
           </label>
           <button type="submit">Add contact</button>
-        </Form>
-      </Formik>
-    </StyledContactForm>
-  );
-};
-
-ContactForm.propTypes = {
-  addContact: PropTypes.func.isRequired,
-};
+        </form>
+      </StyledContactForm>
+    );
+  }
+}
